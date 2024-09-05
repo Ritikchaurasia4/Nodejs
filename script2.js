942a808d9f760eed101c827dad17726163d1ecc2
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-09-05 23:18:09 +0530

    install express and create a server

--- script2.js
+++ script2.js
@@ -1,11 +1,25 @@
-// ======= With the help of this just below syntax we can create a server ......
+// ======= With the help of this just below commented code we can create a server ......
 
-const http = require('http');
-const server = http.createServer(function(req , res){
-    res.end("Hello Server how  are you");
+// const http = require('http');
+// const server = http.createServer(function(req , res){
+//     res.end("Hello Server how  are you");
+// });
+// server.listen(3000 , () =>{
+//     console.log("Server is running on the port 3000")
+// });
+
+
+// in which  we need to install express by the syntax ...   npm i express
+
+const express = require('express');
+const app = express();
+
+app.get("/" , function(req , res){
+    res.send("Backend send the data1");
 });
-server.listen(3000 , () =>{
-    console.log("Server is running on the port 3000")
+app.get("/profile" , function(req , res){
+    res.send("Backend send  data");
 });
-
-
+app.listen(3000 , ()=>{
+    console.log("code is running on port 3000")
+});
